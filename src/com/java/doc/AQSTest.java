0ba25647b00d94db4ed5125e1124c2b080983256
zhf <755109759@qq.com>
2020-12-17 20:34:35 +0800

    aqs源码解析

---
+++ src/com/java/doc/AQSTest.java
@@ -0,0 +1,57 @@
+package com.java.doc;
+
+import com.sun.xml.internal.ws.policy.privateutil.RuntimePolicyUtilsException;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AQSTest {
+
+    public static void main(String[] args) {
+
+        ReentrantLock lock = new ReentrantLock();
+
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                System.out.println("--------A--- come in");
+                TimeUnit.MINUTES.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+
+            }
+
+        }, "A").start();
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                System.out.println("--------B--- come in");
+//                TimeUnit.MINUTES.sleep(2);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+            } finally {
+                lock.unlock();
+
+            }
+        }, "B").start();
+
+        new Thread(() -> {
+            try {
+                lock.lock();
+                System.out.println("--------C--- come in");
+//                TimeUnit.MINUTES.sleep(2);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+            } finally {
+                lock.unlock();
+
+            }
+        }, "C").start();
+
+
+    }
+}
